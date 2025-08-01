aa9ede2b387e54a59d12fec41a4c68ac5727bccc
Niouk971 <florian.polter.77@gmail.com>
2025-08-01 16:07:53 +0200

    Etape 1 terminé

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // A toi de jouer pour cette partie :-) Happy coding !
+
+async function getCoordinates() {
+
+    try {
+
+        const response = await fetch("https://nominatim.openstreetmap.org/search?q=Paris&format=json&addressdetails=1&limit=1");
+
+        if (!response.ok) {
+            throw new Error(`Erreur HTTP : ${response.status}`);
+        }
+        const data = await response.json();
+
+        console.log(data[0].lat);
+
+
+    } catch (error) {
+        console.log(error.message);
+    }
+
+}
+
+getCoordinates();
